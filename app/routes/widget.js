6275a8f2b3fd0893f7d8d0e4c99fe0a8ae28e99f
Gary Stafford <garystafford@rochester.rr.com>
2016-05-30 01:10:10 -0400

    Working with index files and database

--- app/routes/widget.js
+++ app/routes/widget.js
@@ -2,11 +2,19 @@ var mongoose = require('mongoose');
 var Widget = mongoose.model('Widget');
 
 module.exports = function (server) {
+  server.get('/widgets/count', function (req, res, next) {
+    Widget.count(function (err, count) {
+      if (err) return console.error(err);
+      res.send(count.toString());
+    });
+    return next();
+  });
+
   server.get('/widgets', function (req, res, next) {
     Widget.find(function (err, widgets) {
       if (err) return console.error(err);
       res.json(widgets);
-      return next();
     });
+    return next();
   });
 };

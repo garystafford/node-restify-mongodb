8ed65da67b1ba02ca4ac8b13b3d29f9f307ef865
Gary Stafford <garystafford@rochester.rr.com>
2016-05-30 19:07:01 -0400

    Update widget scheama

--- app/models/Widget.js
+++ app/models/Widget.js
@@ -1,11 +1,32 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+var Currency = mongoose.Types.Currency;
 
 var widgetSchema = new Schema({
-  name: String,
-  color: {type: String, enum: ['Red', 'Blue', 'Yellow', 'Green', 'Orange', 'Purple']},
-  size: {type: String, enum: ['Huge', 'Big', 'Medium', 'Small', 'Tiny']},
-  inventory: {type: Number, min: 0},
+  product_id: {type: String, required: true},
+  name: {type: String, required: true},
+  color: {
+    type: String, required: true,
+    enum: ['Red', 'Blue', 'Yellow', 'Green', 'Orange', 'Purple', 'White', 'Black']
+  },
+  size: {
+    type: String, required: true,
+    enum: ['Huge', 'Big', 'Medium', 'Small', 'Tiny']
+  },
+  price: {type: Currency, required: true},
+  inventory: {type: Number, required: true, min: 0},
+});
+
+//Transform
+widgetSchema.set('toJSON', {
+  virtuals: true
+});
+
+widgetSchema.set('toJSON', {
+  transform: function (doc, ret, options) {
+    ret.price = Number(ret.price / 100).toFixed(2);
+  }
 });
 
 var Widget = mongoose.model('Widget', widgetSchema);

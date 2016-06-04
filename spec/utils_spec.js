07975a06a77cbdfafa678feca8e285d2697d6752
Gary Stafford <garystafford@rochester.rr.com>
2016-06-04 00:54:18 -0400

    New Jasmine tests and Postman collection added

--- spec/utils_spec.js
+++ spec/utils_spec.js
@@ -2,20 +2,37 @@ var request = require('request');
 
 var base_url = 'http://localhost:3000';
 
-describe('Widget Application', function () {
+describe('Widget Application Utilities', function () {
   describe('GET /utils/ping', function () {
-    it('returns status code 200', function (done) {
+    it('returns status code of 200', function (done) {
       request.get(base_url + '/utils/ping', function (error, response, body) {
         expect(response.statusCode).toBe(200);
         done();
       });
     });
 
-    it('returns true', function (done) {
+    it('returns true in response body', function (done) {
       request.get(base_url + '/utils/ping', function (error, response, body) {
         expect(body).toBe('true');
         done();
       });
     });
   });
+  describe('GET /utils/info', function () {
+    it('returns status code of 200', function (done) {
+      request.get(base_url + '/utils/info', function (error, response, body) {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+    });
+
+    it('returns info object in response body that matches regular expression', function (done) {
+      request.get(base_url + '/utils/info', function (error, response, body) {
+        // example: {"name":"foo","version":"1.0.0"..."}
+        expect(body).toMatch("(\{\"name\"\:\")(.*)(\"\,\"version\"\:\")(.*)(\"\})");
+        done();
+      });
+    });
+  });
+
 });
